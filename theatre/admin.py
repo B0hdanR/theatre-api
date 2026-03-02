01142147223ebd40807bc808436b30283dd94797
Bohdan Romanov <bytovskyden@gmail.com>
2026-03-02 21:44:27 +0200

    register models in admin

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(TheatreHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
+admin.site.register(Performance)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
