16d9e6926e251a9a09e89d3b22fc4330a8ec41d1
Bohdan Romanov <bytovskyden@gmail.com>
2026-03-04 00:39:03 +0200

    Added standard views for all models

--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,50 @@
 from rest_framework import viewsets
 
-from theatre.models import TheatreHall
-from theatre.serializers import TheatreHallSerializer
+from theatre.models import (
+    TheatreHall,
+    Actor,
+    Genre,
+    Play,
+    Performance,
+    Reservation
+)
+from theatre.serializers import (
+    TheatreHallSerializer,
+    ActorSerializer,
+    GenreSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    ReservationSerializer
+)
 
 
 class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.select_related("play", "theatre_hall")
+    serializer_class = PerformanceSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.prefetch_related(
+        "tickets__performance__play", "tickets__performance__theatre_hall"
+    )
+    serializer_class = ReservationSerializer
