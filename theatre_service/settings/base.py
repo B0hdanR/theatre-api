1535f76266c84259fe83f5fc4a6daea146f72a78
Bohdan Romanov <bytovskyden@gmail.com>
2026-03-06 19:32:03 +0200

    add default permission and pagination classes

--- theatre_service/settings/base.py
+++ theatre_service/settings/base.py
@@ -125,6 +125,11 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle"
